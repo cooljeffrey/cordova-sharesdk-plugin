89e6ee4625ec5714a2d581992d06151c735982ac
Rudy-sun <263290692@qq.com>
2015-05-15 10:55:20 +0800

    Update ShareSdkPlugin.m

--- src/ios/ShareSdkPlugin.m
+++ src/ios/ShareSdkPlugin.m
@@ -7,36 +7,41 @@
 {
     NSMutableDictionary *args = [command.arguments objectAtIndex:0];
     NSString *title = [args objectForKey:@"title"];
-    NSString *description = [args objectForKey:@"description"];
+    // NSString *description = [args objectForKey:@"description"];
     NSString *url = [args objectForKey:@"url"];
     NSString *imageUrl = [args objectForKey:@"imageUrl"];
-    NSString *imageNamed = [args objectForKey:@"imageNamed"];
-
-     
-	//构造分享内容
-	id<ISSContent> publishContent = [ShareSDK content:title
-                                          defaultContent:@"倒不了的塔"
-                                          image:imageUrl
-                                          title:title
-                                          url:@"http://www.sharesdk.cn"
-                                          description:description
-                                          mediaType:SSPublishContentMediaTypeNews];
-
-[ShareSDK showShareActionSheet:nil
-                     shareList:nil
-                     content:publishContent
+    //NSString *imageNamed = [args objectForKey:@"imageNamed"];
+    
+    
+    //构造分享内容
+    id<ISSContent> publishContent = [ShareSDK content:title
+                                       defaultContent:@"最专业的电商平台"
+                                                image:[ShareSDK imageWithUrl:imageUrl]
+                                                title:title
+                                                  url:url
+                                          description:@""
+                                            mediaType:SSPublishContentMediaTypeNews];
+    
+    //创建弹出菜单容器
+    id<ISSContainer> container = [ShareSDK container];
+    //[container setIPadContainerWithView:sender arrowDirect:UIPopoverArrowDirectionUp];
+    //弹出分享菜单
+    [ShareSDK showShareActionSheet:container
+                         shareList:nil
+                           content:publishContent
                      statusBarTips:YES
-                     authOptions:nil
-                     shareOptions: nil
-                     result:^(ShareType type, SSResponseState state, id<ISSPlatformShareInfo> statusInfo, id<ICMErrorInfo> error, BOOL end) {
-                                               if (state == SSResponseStateSuccess)
-                                               {
-                                                       NSLog(@"分享成功");
-                                               }
-                                               else if (state == SSResponseStateFail)
-                                               {
-                                                       NSLog(@"分享失败,错误码:%d,错误描述:%@", [error errorCode], [error errorDescription]);
-                                               }
+                       authOptions:nil
+                      shareOptions:nil
+                            result:^(ShareType type, SSResponseState state, id<ISSPlatformShareInfo> statusInfo, id<ICMErrorInfo> error, BOOL end) {
+                                
+                                if (state == SSResponseStateSuccess)
+                                {
+                                    NSLog(NSLocalizedString(@"TEXT_ShARE_SUC", @"分享成功"));
+                                }
+                                else if (state == SSResponseStateFail)
+                                {
+                                    NSLog(NSLocalizedString(@"TEXT_ShARE_FAI", @"分享失败,错误码:%d,错误描述:%@"), [error errorCode], [error errorDescription]);
+                                }
                             }];
 }
 
